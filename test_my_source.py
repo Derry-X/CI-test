8381c82ef13b55e9b99ad493d971174ffba044b4
Derry-X <85951211+Derry-X@users.noreply.github.com>
2021-06-15 18:10:55 +0200

    Update test_my_source.py

--- test_my_source.py
+++ test_my_source.py
@@ -3,9 +3,9 @@ from my_source import add_func
 
 
 def test_add_one():
-    assert add_func(2, 5) == 7
+    assert add_func(2, 5) == 8
 
 
 @pytest.mark.xfail
 def test_add_fail():
-    assert add_func(5, 5) == 10
+    assert add_func(5, 5) == 9
